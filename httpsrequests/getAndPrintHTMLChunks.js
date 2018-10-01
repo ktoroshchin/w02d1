80e320dc4fb7a48b966df86216cd9ec96b19e550
Konstantin Toroshchin <k.toroshchin@gmail.com>
2018-10-01 15:25:45 +0000

    printing with buffered string

---
+++ httpsrequests/getAndPrintHTMLChunks.js
@@ -0,0 +1,33 @@
+// while https is built-in to Node, it is a module, so it must be required
+var https = require('https');
+
+// the host can be thought of as the domain name you want to read from,
+// and the path is the resource - '/' is the root path, but if you wanted to read a
+// particular resource (like '/login/index.html'), that would be defined in the path
+function getAndPrintHTMLChunks () {
+
+
+  var requestOptions = {
+    host: 'example.com',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function (response) {
+
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      console.log('Chunk Received. Length:', data + "\n");
+    });
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('error', function() {
+      console.log('Error reading data');
+    });
+  });
+};
+
+getAndPrintHTMLChunks();
